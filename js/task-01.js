11cdcc25c955d581dbe71b6768921922ed9b468e
YeliseyevaO <eliseevaolgaf5@gmail.com>
2021-07-03 23:47:24 +0300

    задание 01

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const categorsEl = document.querySelectorAll('.item')
+const categorAmount =categorsEl.length
+console.log(`В списке ${categorAmount} категории.`)
+
+const titlesEl = document.querySelectorAll('h2');
+console.log(titlesEl);
+const listEl = document.querySelectorAll('.item li')
+console.log(listEl)
